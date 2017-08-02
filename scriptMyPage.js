b636e07ab965278b66254c468058a9b40af7c007
Nisha Pant <nisha007pant@gmail.com>
2017-08-02 13:29:46 -0500

    added objects

--- scriptMyPage.js
+++ scriptMyPage.js
@@ -1,14 +1,41 @@
-
-class myTask{
+var taskArray = [];
+class MyTask{
   constructor(taskName, pointValue){
     this.taskName = taskName;
     this.pointValue = pointValue;
   }
 
+  printName(){
+    return this.taskName;
+  }
+
 }
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+taskArray.push(new MyTask("Bring a reusable bottle", 0));
+
 
 var totalpoints = 0
 function addPoints(pointVal){
   totalpoints += pointVal;
   alert(totalpoints);
+  alert(taskArray[0].printName());
 }
