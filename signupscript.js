c01e06a35471de01dbc13ce1f11821f58a3fba5b
Nisha Pant <nisha007pant@gmail.com>
2017-08-02 21:41:45 -0500

    Add files via upload

---
+++ signupscript.js
@@ -0,0 +1,31 @@
+var peopleInfo = [];
+var users = [];
+
+
+class Person{
+  constructor(username, password, email){
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+  getUsername(){
+    return this.username;
+  }
+  getPassword(){
+    return this.password;
+  }
+  getEmail(){
+    return this.email;
+  }
+}
+
+function collectData(){
+  var user, psswrd, mail;
+  alert(users);
+  user = document.getElementById("username").value;
+  psswrd = document.getElementById("password").value;
+  mail = document.getElementById("email").value;
+  peopleInfo.push(new Person(user, psswrd, mail));
+  alert(peopleInfo);
+
+}
