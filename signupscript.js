983069fcbb785c3f7fef3d9c83ccaf054f3e6756
Nisha Pant <nisha007pant@gmail.com>
2017-08-04 13:40:53 -0500

    added group picture

--- signupscript.js
+++ signupscript.js
@@ -1,25 +1,25 @@
-var peopleInfo = [];
-
-
-class Person{
-  constructor(username, password, email){
-    this.username = username;
-    this.password = password;
-    this.email = email;
-  }
-  getUsername(){
-    return this.username;
-  }
-  getPassword(){
-    return this.password;
-  }
-  getEmail(){
-    return this.email;
-  }
-}
-
-function collectData(){
-  var user, psswrd, mail;
-  user = document.getElementById("username").value;
-  alert(peopleInfo[0].getUsername());
-}
+// var peopleInfo = [];
+//
+//
+// class Person{
+//   constructor(username, password, email){
+//     this.username = username;
+//     this.password = password;
+//     this.email = email;
+//   }
+//   getUsername(){
+//     return this.username;
+//   }
+//   getPassword(){
+//     return this.password;
+//   }
+//   getEmail(){
+//     return this.email;
+//   }
+// }
+//
+// function collectData(){
+//   var user, psswrd, mail;
+//   user = document.getElementById("username").value;
+//   alert(peopleInfo[0].getUsername());
+// }
